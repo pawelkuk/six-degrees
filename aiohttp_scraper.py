2cf4f929827aa32ac8f8ef1fe0c20e63c004df00
Paweł Kukliński <paffcio314@gmail.com>
2020-01-26 21:39:01 +0100

    Add asyncio web crawler based on aiohttp framework.
    To run the script type: python aiohttp_scraper

--- aiohttp_scraper.py
+++ aiohttp_scraper.py
@@ -7,18 +7,34 @@ import asyncio
 import aiohttp
 from bs4 import BeautifulSoup
 from re import compile
-
+import functools
 
 BASE_URL = "https://en.wikipedia.org"
 PATTERN = "^/wiki/(?![a-zA-Z]+:)"
 
+count = 0
 
-async def get_page(url, session):
-    response = await session.get(url)
-    if response.status == 200:
-        return await get_wiki_page(response)
-    else:
-        return None
+
+def debug(func):
+    @functools.wraps(func)
+    async def wrapper(*args, **kwargs):
+        global count
+        count += 1
+        tmp = count
+        print(f"{tmp}. Function {func.__name__} started executing")
+        res = await func(*args, **kwargs)
+        print(f"{tmp}. Function {func.__name__} ended executing")
+        return res
+
+    return wrapper
+
+
+@debug
+async def download_wiki_html(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            content = await resp.text()
+            return get_wiki_page(content, resp)
 
 
 @dataclass
@@ -28,39 +44,42 @@ class Page:
     links: List[str]
 
 
-async def get_wiki_page(response):
-    soup = BeautifulSoup(await response.text(), "html.parser")
-    urls = soup.find(id="content").findAll(
-        "a", attrs={"href": compile(PATTERN)}
-    )
-    valid_urls: Iterator[str] = map(
-        lambda x: urljoin(BASE_URL, x.get("href")), urls
-    )
-    title = soup.find(id="firstHeading").get_text()
-    return Page(title, str(response.url), list(valid_urls)).__dict__
+def get_wiki_page(content, response):
+    title = None
+    valid_urls = []
+    if content:
+        soup = BeautifulSoup(content, "html.parser")
+        urls = soup.find(id="content").findAll(
+            "a", attrs={"href": compile(PATTERN)}
+        )
+        valid_urls: Iterator[str] = map(
+            lambda x: urljoin(BASE_URL, x.get("href")), urls
+        )
+        title = soup.find(id="firstHeading").get_text()
+    return Page(title or "", str(response.url), list(valid_urls)).__dict__
 
 
 async def async_download_pages(source_, target, field, **kwargs):
-    async with aiohttp.ClientSession() as session:
-        target = await get_page(target, session)
-        source_ = await get_page(source_, session)
-        input_pages = [source_]
-        all_pages = [source_]
-        iteration = 0
-        while True:
-            print("**")
-            if (
-                any([target[field] in page["links"] for page in input_pages])
-                or iteration == 6
-            ):
-                break
-            links = [l for page in input_pages if page for l in page["links"]]
-            input_pages_coros = [await session.get(link) for link in links]
-            input_pages = [
-                await get_wiki_page(resp) for resp in input_pages_coros
-            ]
-            all_pages.extend(input_pages)
-            iteration += 1
+    target, source_ = await asyncio.gather(
+        *[download_wiki_html(target), download_wiki_html(source_)]
+    )
+    input_pages = [source_]
+    all_pages = [source_]
+    iteration = 0
+    while True:
+        print("**")
+        if (
+            any([target[field] in page["links"] for page in input_pages])
+            or iteration == 6
+        ):
+            break
+        links = [l for page in input_pages if page for l in page["links"]]
+        print(f"There are {len(links)} to scrape...")
+        input_pages = await asyncio.gather(
+            *[download_wiki_html(link) for link in links]
+        )
+        all_pages.extend(input_pages)
+        iteration += 1
     return all_pages
 
 
@@ -68,7 +87,6 @@ if __name__ == "__main__":
     # source_url = "https://en.wikipedia.org/wiki/Adolf_Hitler"
     # target_url = "https://en.wikipedia.org/wiki/Nazi_Party"
 
-    loop = asyncio.get_event_loop()
     target_url = (
         "https://en.wikipedia.org/wiki/Conditional_probability_distribution"
     )
@@ -76,9 +94,6 @@ if __name__ == "__main__":
     source_url = "https://en.wikipedia.org/wiki/Channel_capacity"
 
     start = time()
-    loop = asyncio.get_event_loop()
-    pages = loop.run_until_complete(
-        async_download_pages(source_url, target_url_2, "url")
-    )
+    pages = asyncio.run(async_download_pages(source_url, target_url_2, "url"))
     print(pages)
     print(time() - start)
